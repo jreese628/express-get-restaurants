286b0992db2b5014fd8aaadad8910ee8afece183
Jeffrey Reese <jreese628@yahoo.com>
2024-02-21 14:29:21 -0500

    CRUD additions

--- src/app.js
+++ src/app.js
@@ -1,14 +1,45 @@
 const express = require("express");
 const app = express();
-const Restaurant = require("../models/index")
+const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
-//TODO: Create your GET Request Route Below: 
+//TODO: Create your GET Request Route Below:
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/restaurants", async (req, res) => {
-    const restaurants = await Restaurant.findAll();
-    res.json(restaurants);
-  });
+  const restaurants = await Restaurant.findAll();
+  res.json(restaurants);
+});
 
+app.get("/restaurants/:id", async (req, res) => {
+  const id = req.params.id;
+  const restaurant = await Restaurant.findByPk(id);
+  res.json(restaurant);
+});
+
+app.post("/restaurants", async (req, res) => {
+  const newRestaurant = req.body;
+  const createdRestaurant = await Restaurant.create(newRestaurant);
+  res.json(createdRestaurant)
+});
+
+app.put("/restaurants/:id", async (req, res) => {
+  const id = req.params.id
+  const updatedRestaurantData = req.body;
+
+  const restaurant = await Restaurant.findByPk(id);
+  const updatedRestaurant = await restaurant.update(updatedRestaurantData);
+  res.json(updatedRestaurant);
+})
+
+app.delete("/restaurants/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const restaurant = await Restaurant.findByPk(id);
+  await restaurant.destroy();
+  res.status(204).send()
+});
 
 module.exports = app;
